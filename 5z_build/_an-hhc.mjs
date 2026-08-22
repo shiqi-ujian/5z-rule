76f35dd9e73c1a14d9d0f89f686a0ea6564f4a64
shiqi-ujian <3446744535@qq.com>
2026-08-23 01:33:30 +0800

    chore: 移除附录拆分调试脚本

--- 5z_build/_an-hhc.mjs
+++
@@ -1,7 +0,0 @@
-// 查看 HHC 中「盗贼」条目的结构（插入子条目用）
-import fs from 'node:fs';
-import { TextDecoder } from 'node:util';
-const text = new TextDecoder('gb18030').decode(fs.readFileSync('5z_src/5z规则1.59版.hhc'));
-const idx = text.indexOf('职业/盗贼.htm');
-console.log('盗贼 @', idx, '/', text.length);
-console.log(text.slice(idx - 500, idx + 400));
--- 5z_build/_chk-charset.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-const { TextDecoder } = require('util');
-for (const f of ['契术师.htm','盗贼.htm','召唤师.htm']) {
-  const buf = fs.readFileSync('5z_src/职业/' + f);
-  const head = buf.slice(0, 800).toString('latin1');
-  const m = /charset\s*=\s*["']?([^"'\s>]+)/i.exec(head);
-  console.log(f, 'charset 声明:', m ? m[1] : '(无)');
-}
